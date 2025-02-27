b6fa5ad04655b3cd306f807b08aaa08253f0bf2f
Erlangga Tresnamada Muliawan <tresnamada@gmail.com>
2025-02-27 16:01:50 +0700

    (+)Komunitas,HomePage Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import HomePage from './components/HomePage';
 import Navbar from './components/Section/Navbar';
+// import Community from './components/CommunityPage';
+
 
 function App() {
   return (
     <div className="relative min-h-screen bg-[#EBE3D5]">
       <Navbar />
+        {/* <Community /> */}
       <main className="pt-24 pb-24 md:pt-28 md:pb-0">
         <HomePage />
       </main>
